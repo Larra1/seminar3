f0d50f3a5b5c66e8b3380454364116608f3e639a
Ivan Odobesko <odobesko.ivan@gmail.com>
2023-06-02 19:35:57 +0300

    эПерваяэ

---
+++ dz1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int H = Convert.ToInt32(Console.ReadLine());
+
+int A = H/10000;
+int B = H/1000%10;
+int D = H/10%10;
+int E = H%10;
+
+// Console.WriteLine(A);
+// Console.WriteLine(B);
+// Console.WriteLine(D);
+// Console.WriteLine(E);
+
+if (A==E && B==D)
+{
+    Console.WriteLine("Палиндром");
+}
+
+else
+{
+    Console.WriteLine("Не палиндром");
+}
