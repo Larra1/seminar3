5ff9635b2899289e421e92ec2b32c86575e0abab
Ivan Odobesko <odobesko.ivan@gmail.com>
2023-06-02 19:43:29 +0300

    Третья

---
+++ dz3/Program.cs
@@ -0,0 +1,17 @@
+﻿// 
+Console.WriteLine("Введите число N");
+int A = Convert.ToInt32(Console.ReadLine());
+if (A<=0)
+{
+    Console.WriteLine("Нет");
+}
+
+else 
+{
+
+for (int i = 1; i<=A; i++ )
+{
+    int B = i*i*i;
+    Console.WriteLine(B);
+}
+}
